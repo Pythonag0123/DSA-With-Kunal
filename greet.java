dc3c1722b6557832985180d8ceb7c2e255b783e6
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2024-07-24 20:22:17 +0530

    Add files via upload

---
+++ greet.java
@@ -0,0 +1,14 @@
+package Assignment1;
+import java.util.*;
+
+
+public class greet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        System.out.println("HELLOO " + name + "!");
+        sc.close();
+    }
+    
+}
